1ed00188d782656899dd9cc0966c5769f76ba6d3
Bertho <james.bertho94@gmail.com>
2020-07-09 17:45:38 +0200

    add and delete dynamicly

--- app/services/championsService.py
+++ app/services/championsService.py
@@ -2,11 +2,30 @@ from app.project_models.selectedChampion import SelectedChampion
 
 
 def addSelectedChampions(selectedChampions, champion):
-    print(champion.name)
-    print(champion.traits)
-    print(champion.cost)
-    print(champion.pngPath)
     selectedChampion = SelectedChampion(champion.name, champion.cost, champion.traits, champion.pngPath)
     selectedChampions.append(selectedChampion)
 
     return selectedChampions
+
+
+def can_add_selected_champion(selected_champions, name):
+    if len([x for x in selected_champions if x.name == name]) > 0:
+        return False
+    if len(selected_champions) > 8:
+        return False
+
+    return True
+
+
+def get_selected_champion(selected_champions, name):
+    return [x for x in selected_champions if x.name == name][0]
+
+
+def reset_champion_remaining(champion_by_cost, selected_champion: SelectedChampion):
+    cost = selected_champion.cost
+    champion_by_cost[cost-1] = champion_by_cost[cost-1] - selected_champion.playerCount
+    if champion_by_cost[cost - 1] < 0:
+        champion_by_cost[cost - 1] = 0
+    return champion_by_cost
+
+
--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('game/championsByCost/update', views.update_champion_bought, name='update_champion_bought'),
     path('game/selectedChamp/add', views.new_selected_champion, name='new_selected_champion'),
+    path('game/selectedChamp/delete', views.remove_selected_champion, name='remove_selected_champion'),
     path('graphStats/', views.json_example, name='json_example'),
     path('game/', views.game, name='game'),
 ]
